b45674c1853258c4fe3ebad51d91034b6f9e64c7
dneva <47559601+dneva@users.noreply.github.com>
2019-04-23 17:10:50 +0300

    Update main.cpp

--- lab1.1/main.cpp
+++ lab1.1/main.cpp
@@ -1,16 +1,61 @@
-// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include "pch.h"
 #include "Complex.h"
 #include <iostream>
 using namespace std;
 int main()
 {
-	Complex c1, c2;
-	cin >> c1 >> c2;
-	c1 = c1 + c2;
-	cout << c1;
+	Complex a, b;
+	int sw = 1;
+	cout << "1 - sum, 2 - minus, 3 - multiply, 4 - divide,  0 - exit" << endl;//выбор
+	cin >> sw;
+	while (sw != 0)
+	{
+		switch (sw)
+		{
+		case 1://сложение
+		{
+			cout << "Input a,b" << endl;
+			cin >> a >> b;
+			cout << a << "+" << b << "=";
+			cout<< a + b << endl;
+		}
+		break;
+		case 2://вычитание
+		{
+			cout << "Input a,b" << endl;
+			cin >> a >> b;
+			cout << a << "-" << b << "=";
+			cout<< a - b << endl;
+		}
+		break;
+		case 3://умножение
+		{
+			cout << "Input a,b" << endl;
+			cin >> a >> b;
+			cout << a << "*" << b << "=";
+			cout << a * b << endl;
+		}
+		break;
+		case 4://деление
+		{
+			cout << "Input a,b" << endl;
+			cin >> a >> b;
+			cout << a << "/" << b << "=";
+			cout << a / b << endl;
+		}
+		break;
+		case 0://выход из цикла
+			break;
+		default:
+			cout << "Repeat input" << endl;
+			break;
+		}
+		cout << "1 - sum, 2 - minus, 3 - multiply, 4 - divide,  0 - exit" << endl;
+		cin >> sw;
+	}
+	system("pause");
+	return 0;
+
 
 }
 
