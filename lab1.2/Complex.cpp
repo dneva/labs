5d55538ed5e361fcd1ba9116cb4e71a343542f4d
dneva <47559601+dneva@users.noreply.github.com>
2019-04-23 17:39:52 +0300

    Create Complex.cpp

---
+++ lab1.2/Complex.cpp
@@ -0,0 +1,22 @@
+#ifndef Complex_H
+#define Complex_H
+#include<iostream>
+using namespace std;
+class Complex
+{
+		private:
+			double real;
+			double img;
+		public:
+			Complex(double r=0, double i=0);
+			void define(double r = 0, double i = 0);
+			double module();
+			Complex& operator=(const Complex& right);
+			friend Complex operator+ (Complex& left, Complex& right);
+			friend Complex operator- (Complex& left, Complex& right);
+			friend Complex operator*(Complex& left, Complex& right);
+			friend Complex operator/(Complex& left, Complex& right);
+			friend ostream& operator<<(ostream& out, const Complex& c);
+			friend istream& operator>>(istream& in, Complex& c);
+};
+#endif
