63bf451c84f01ecf2defe11cd71196db52e41576
dneva <47559601+dneva@users.noreply.github.com>
2019-04-23 16:56:23 +0300

    Update Complex.h

--- lab1.1/Complex.h
+++ lab1.1/Complex.h
@@ -11,9 +11,10 @@ class Complex
 			Complex(double r=0, double i=0);
 			Complex& operator=(const Complex& right);
 			friend Complex operator+ (Complex& left, Complex& right);
+			friend Complex operator- (Complex& left, Complex& right);
+			friend Complex operator*(Complex& left, Complex& right);
+			friend Complex operator/(Complex& left, Complex& right);
 			friend ostream& operator<<(ostream& out, const Complex& c);
 			friend istream& operator>>(istream& in, Complex& c);
 };
-
-
 #endif
