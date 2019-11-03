6aca995050656bd42327145afebbb5319e9e2db9
dneva <47559601+dneva@users.noreply.github.com>
2019-11-03 18:54:42 +0300

    Create Main.java

---
+++ java/labs/6/Main.java
@@ -0,0 +1,12 @@
+package Lab6;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BorderPanels bp =  new BorderPanels();
+        bp.setVisible(true);
+	    bp.setDefaultCloseOperation(bp.EXIT_ON_CLOSE);
+    }
+}
