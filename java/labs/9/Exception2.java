a3bb83ba984a9cac4ee82244b34c6c557bf129c6
dneva <47559601+dneva@users.noreply.github.com>
2019-11-25 20:39:07 +0300

    Create Exception2.java

---
+++ java/labs/9/Exception2.java
@@ -0,0 +1,23 @@
+package Lab9;
+
+import java.util.Scanner;
+
+public class Exception2 {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter an integer ");
+        String intString = myScanner.next();
+        try {
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка в формате введенного числа");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Деление на 0");
+        }
+    }
+}
+
