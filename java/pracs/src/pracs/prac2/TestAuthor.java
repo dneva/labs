95189083155550f74e6b39ffa826a1f24dede899
dneva <dtkkkmt@mail.ru>
2019-12-11 23:44:15 +0300

    commit

---
+++ java/pracs/src/pracs/prac2/TestAuthor.java
@@ -0,0 +1,9 @@
+package pracs.prac2;
+
+public class TestAuthor {
+
+    public static void main(String[] args) {
+	    Author a = new Author("Ivan Popov","ivPopov@somewhere.com",'m');
+	    System.out.println(a);
+    }
+}
