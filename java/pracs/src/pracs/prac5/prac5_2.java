1ae5664e015932e1f04bd79545e20198ec73023b
dneva <dtkkkmt@mail.ru>
2019-12-18 23:06:56 +0300

    commit

--- java/pracs/src/pracs/prac5/prac5_2.java
+++ java/pracs/src/pracs/prac5/prac5_2.java
@@ -1,12 +1,20 @@
 package pracs.prac5;
 
+import java.util.Scanner;
+
 public class prac5_2 {
     public static int func(int n){
-        if (n==1)
+        if(n==1) {
+            System.out.print(1+" ");
             return 1;
-return func(n-1);
+        }
+       func(n-1);
+        System.out.print(n+" ");
+        return 1;
+
     }
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        func(s.nextInt());
     }
 }
