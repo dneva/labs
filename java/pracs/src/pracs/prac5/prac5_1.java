22a3a565543c60749839f945c586f3c73f803a06
dneva <dtkkkmt@mail.ru>
2019-12-18 22:35:59 +0300

    commit

---
+++ java/pracs/src/pracs/prac5/prac5_1.java
@@ -0,0 +1,22 @@
+package pracs.prac5;
+
+import java.util.Scanner;
+
+//Дана монотонная последовательность, в которой каждое натуральное число k встречается ровно k раз: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,…
+public class prac5_1 {
+    public static int func(int n)
+    {
+        if(n==1) {
+            System.out.print(1);
+            return 1;
+        }
+        for(int i=0;i<n;i++)
+            System.out.print(n+" ");
+        func(n-1);
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        func(s.nextInt());
+    }
+}
